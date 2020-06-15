e4e5b4eb7c701eb54db2d937d63d4876781dfaf2
moses ubah <39748749+mohzys23@users.noreply.github.com>
2020-06-15 15:32:00 +0000

    Update script.js

--- pricing-component-with-toggle-master/script.js
+++ pricing-component-with-toggle-master/script.js
@@ -1,13 +1,22 @@
-function switchVisible() {
-            if (document.getElementById('Div1')) {
-
-                if (document.getElementById('Div1').style.display == 'none') {
+var display = window.innerWidth;
+function switchVisible(){
+     if(display < 440 ){
+            if (document.getElementById('Div1').style.display == 'none') {
+                document.getElementById('Div1').style.display = 'block';
+                document.getElementById('Div2').style.display = 'none';
+            } else {
+                document.getElementById('Div1').style.display = 'none';
+                document.getElementById('Div2').style.display = 'block';
+            }
+     }else{
+       if (document.getElementById('Div1')) {
+            if (document.getElementById('Div1').style.display == 'none') {
                     document.getElementById('Div1').style.display = 'flex';
                     document.getElementById('Div2').style.display = 'none';
-                }
-                else {
+            }else {
                     document.getElementById('Div1').style.display = 'none';
                     document.getElementById('Div2').style.display = 'flex';
-                }
             }
+        }
+    }
 }
